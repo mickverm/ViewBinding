840f2e43abfeb3a1f6e7d7c28b9af1a5e2eacd90
Michiel Vermeersch <mv@unmatched.eu>
2019-07-04 14:45:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+Viewbinding
+===========
+[ ![Download](https://api.bintray.com/packages/mickverm/maven/Viewbinding/images/download.svg) ](https://bintray.com/mickverm/maven/Viewbinding/_latestVersion)
+
+[ButterKnife][1]-esque view binding for Kotlin based on [KotterKnife][2].
+
+```kotlin
+public class PersonView(context: Context, attrs: AttributeSet?) : LinearLayout(context, attrs){
+	val firstName: TextView by bindView(R.id.tv_first_name)
+	val lastName by bindView<TextView>(R.id.tv_last_name)
+	
+	// Optional binding
+	val middleName: TextView? by bindOptionalView(R.id.tv_middle_name)
+	
+	// List binding
+	val nameViews: List<TextView> by bindViews(R.id.tv_first_name, R.id.tv_last_name)
+	
+	// List binding with optional items being omitted
+	val nameViews: List<TextView> by bindOptionalViews(R.id.tv_first_name, R.id.tv_middle_name, R.id.tv_last_name)
+}
+```
+
+These methods are available on subclasses of `View`, `Activity`, `Dialog`, `DialogFragment`, `Fragment` and RecyclerView's `ViewHolder`.
+
+Download
+--------
+
+Gradle:
+````groovy
+implementation 'be.mickverm.viewbinding:viewbinding:1.0.0'
+````
+
+or Maven:
+````xml
+<dependency>
+  <groupId>be.mickverm.viewbinding</groupId>
+  <artifactId>viewbinding</artifactId>
+  <version>1.0.0</version>
+  <type>pom</type>
+</dependency>
+````
+
+License
+-------
+
+    Copyright 2019 Michiel Vermeersch
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+
+[1]: https://jakewharton.github.io/butterknife
+[2]: https://github.com/JakeWharton/kotterknife
---
+++ viewbinding/src/main/java/be/mickverm/viewbinding/ViewBinding.kt
@@ -0,0 +1,198 @@
+package be.mickverm.viewbinding
+
+import android.app.Activity
+import android.app.Dialog
+import android.view.View
+import androidx.annotation.IdRes
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.Observer
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+
+fun <V : View> View.bindView(@IdRes id: Int): ReadOnlyProperty<View, V> =
+    required(id, viewFinder)
+
+fun <V : View> View.bindViews(vararg ids: Int): ReadOnlyProperty<View, List<V>> =
+    required(ids, viewFinder)
+
+fun <V : View> View.bindOptionalView(@IdRes id: Int): ReadOnlyProperty<View, V?> =
+    optional(id, viewFinder)
+
+fun <V : View> View.bindOptionalViews(vararg ids: Int): ReadOnlyProperty<View, List<V>> =
+    optional(ids, viewFinder)
+
+
+fun <V : View> Activity.bindView(@IdRes id: Int): ReadOnlyProperty<Activity, V> =
+    required(id, viewFinder)
+
+fun <V : View> Activity.bindViews(vararg ids: Int): ReadOnlyProperty<Activity, List<V>> =
+    required(ids, viewFinder)
+
+fun <V : View> Activity.bindOptionalView(@IdRes id: Int): ReadOnlyProperty<Activity, V?> =
+    optional(id, viewFinder)
+
+fun <V : View> Activity.bindOptionalViews(vararg ids: Int): ReadOnlyProperty<Activity, List<V>> =
+    optional(ids, viewFinder)
+
+
+fun <V : View> Dialog.bindView(@IdRes id: Int): ReadOnlyProperty<Dialog, V> =
+    required(id, viewFinder)
+
+fun <V : View> Dialog.bindViews(vararg ids: Int): ReadOnlyProperty<Dialog, List<V>> =
+    required(ids, viewFinder)
+
+fun <V : View> Dialog.bindOptionalView(@IdRes id: Int): ReadOnlyProperty<Dialog, V?> =
+    optional(id, viewFinder)
+
+fun <V : View> Dialog.bindOptionalViews(vararg ids: Int): ReadOnlyProperty<Dialog, List<V>> =
+    optional(ids, viewFinder)
+
+
+fun <V : View> DialogFragment.bindView(@IdRes id: Int): ReadOnlyProperty<DialogFragment, V> =
+    required(id, viewFinder)
+
+fun <V : View> DialogFragment.bindViews(vararg ids: Int): ReadOnlyProperty<DialogFragment, List<V>> =
+    required(ids, viewFinder)
+
+fun <V : View> DialogFragment.bindOptionalView(@IdRes id: Int): ReadOnlyProperty<DialogFragment, V?> =
+    optional(id, viewFinder)
+
+fun <V : View> DialogFragment.bindOptionalViews(vararg ids: Int): ReadOnlyProperty<DialogFragment, List<V>> =
+    optional(ids, viewFinder)
+
+
+fun <V : View> Fragment.bindView(@IdRes id: Int): ReadOnlyProperty<Fragment, V> =
+    required(this, id, viewFinder)
+
+fun <V : View> Fragment.bindViews(vararg ids: Int): ReadOnlyProperty<Fragment, List<V>> =
+    required(this, ids, viewFinder)
+
+fun <V : View> Fragment.bindOptionalView(@IdRes id: Int): ReadOnlyProperty<Fragment, V?> =
+    optional(this, id, viewFinder)
+
+fun <V : View> Fragment.bindOptionalViews(vararg ids: Int): ReadOnlyProperty<Fragment, List<V>> =
+    optional(this, ids, viewFinder)
+
+
+fun <V : View> RecyclerView.ViewHolder.bindView(@IdRes id: Int): ReadOnlyProperty<RecyclerView.ViewHolder, V> =
+    required(id, viewFinder)
+
+fun <V : View> RecyclerView.ViewHolder.bindViews(vararg ids: Int): ReadOnlyProperty<RecyclerView.ViewHolder, List<V>> =
+    required(ids, viewFinder)
+
+fun <V : View> RecyclerView.ViewHolder.bindOptionalView(@IdRes id: Int): ReadOnlyProperty<RecyclerView.ViewHolder, V?> =
+    optional(id, viewFinder)
+
+fun <V : View> RecyclerView.ViewHolder.bindOptionalViews(vararg ids: Int): ReadOnlyProperty<RecyclerView.ViewHolder, List<V>> =
+    optional(ids, viewFinder)
+
+
+private val View.viewFinder: View.(Int) -> View?
+    get() = { findViewById(it) }
+
+private val Activity.viewFinder: Activity.(Int) -> View?
+    get() = { findViewById(it) }
+
+private val Dialog.viewFinder: Dialog.(Int) -> View?
+    get() = { findViewById(it) }
+
+private val DialogFragment.viewFinder: DialogFragment.(Int) -> View?
+    get() = { dialog?.findViewById(it) ?: view?.findViewById(it) }
+
+private val Fragment.viewFinder: Fragment.(Int) -> View?
+    get() = { view!!.findViewById(it) }
+
+private val RecyclerView.ViewHolder.viewFinder: RecyclerView.ViewHolder.(Int) -> View?
+    get() = { itemView.findViewById(it) }
+
+
+private fun viewNotFound(id: Int, desc: KProperty<*>): Nothing =
+    throw IllegalStateException("View ID $id for '${desc.name}' not found.")
+
+
+@Suppress("UNCHECKED_CAST")
+private fun <T, V : View> required(id: Int, finder: T.(Int) -> View?) =
+    Lazy { t: T, desc -> t.finder(id) as V? ?: viewNotFound(id, desc) }
+
+@Suppress("UNCHECKED_CAST")
+private fun <T, V : View> required(ids: IntArray, finder: T.(Int) -> View?) =
+    Lazy { t: T, desc -> ids.map { t.finder(it) as V? ?: viewNotFound(it, desc) } }
+
+@Suppress("UNCHECKED_CAST")
+private fun <T, V : View> optional(id: Int, finder: T.(Int) -> View?) =
+    Lazy { t: T, _ -> t.finder(id) as V? }
+
+@Suppress("UNCHECKED_CAST")
+private fun <T, V : View> optional(ids: IntArray, finder: T.(Int) -> View?) =
+    Lazy { t: T, _ -> ids.map { t.finder(it) as V? }.filterNotNull() }
+
+
+@Suppress("UNCHECKED_CAST")
+private fun <V : View> required(fragment: Fragment, id: Int, finder: Fragment.(Int) -> View?) =
+    LazyFragment(fragment) { t: Fragment, desc -> t.finder(id) as V? ?: viewNotFound(id, desc) }
+
+@Suppress("UNCHECKED_CAST")
+private fun <V : View> required(fragment: Fragment, ids: IntArray, finder: Fragment.(Int) -> View?) =
+    LazyFragment(fragment) { t: Fragment, desc -> ids.map { t.finder(it) as V? ?: viewNotFound(it, desc) } }
+
+@Suppress("UNCHECKED_CAST")
+private fun <V : View> optional(fragment: Fragment, id: Int, finder: Fragment.(Int) -> View?) =
+    LazyFragment(fragment) { t: Fragment, _ -> t.finder(id) as V? }
+
+@Suppress("UNCHECKED_CAST")
+private fun <V : View> optional(fragment: Fragment, ids: IntArray, finder: Fragment.(Int) -> View?) =
+    LazyFragment(fragment) { t: Fragment, _ -> ids.map { t.finder(it) as V? }.filterNotNull() }
+
+
+private class Lazy<T, V>(
+    private val initializer: (T, KProperty<*>) -> V
+) : ReadOnlyProperty<T, V> {
+    private object EMPTY
+
+    private var value: Any? = EMPTY
+
+    override fun getValue(thisRef: T, property: KProperty<*>): V {
+        if (value == EMPTY) {
+            value = initializer(thisRef, property)
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return value as V
+    }
+}
+
+private class LazyFragment<V>(
+    fragment: Fragment,
+    private val initializer: (Fragment, KProperty<*>) -> V
+) : ReadOnlyProperty<Fragment, V>, LifecycleObserver {
+    private object EMPTY
+
+    private var value: Any? = EMPTY
+
+    init {
+        fragment.viewLifecycleOwnerLiveData.observe(fragment, Observer {
+            it.lifecycle.addObserver(this)
+        })
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): V {
+        if (value == EMPTY) {
+            value = initializer(thisRef, property)
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return value as V
+    }
+
+    @Suppress("unused")
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onViewDestroyed() {
+        value = EMPTY
+    }
+}
